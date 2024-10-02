6b86cd4aed672e8cb4519b4e02fa002388319400
From-Sochi <blinov.e123@gmail.com>
2024-10-02 13:21:36 +0300

    inithial

---
+++ my-app/src/app.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { GlobalStyle } from './assets/styles/app.styles';
+
+
+
+
+
+function App() {
+    return (
+        <>
+            <GlobalStyle />
+            привет!
+        </>
+    );
+}
+
+export default App;
---
+++ my-app/src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './app';
+import { GlobalStyle } from './assets/styles/app.styles';
+const root = ReactDOM.createRoot(
+    document.getElementById('root') as HTMLElement
+);
+root.render(
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>
+);
