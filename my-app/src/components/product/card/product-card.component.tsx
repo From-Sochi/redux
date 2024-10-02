4f1285df1809177c53881326f9d8e809b2257a77
From-Sochi <blinov.e123@gmail.com>
2024-10-02 15:06:13 +0300

    раскидал архитектуру проекта

---
+++ my-app/src/components/product/card/product-card.component.tsx
@@ -0,0 +1,6 @@
+import React, { memo, FC } from 'react';
+type ProductCardProps = {};
+const ProductCard: FC<ProductCardProps> = () => {
+    return <div>ProductCard</div>;
+};
+export default memo(ProductCard);
